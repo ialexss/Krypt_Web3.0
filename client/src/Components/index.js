98ecaa4327c8e2467412d8cba0c95498f778cd5c
ialexss <alexandersanchezsalces@gmail.com>
2023-08-22 14:34:30 -0400

    created components

---
+++ client/src/Components/index.js
@@ -0,0 +1,8 @@
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Welcome from "./Welcome";
+import Transactions from "./Transactions";
+import Services from "./Services";
+
+export {Loader,Navbar,Footer,Welcome,Transactions,Services}
